f92a73e12bc451a72d7b8e272a70569e75b035a6
Koti Syamala <kotireddy299@gmail.com>
2021-12-03 07:42:58 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer class="py-5 bg-primary fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; PERCIS 2021</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
